abb96755a81f48e52b474a6f9470f99fb54b6748
Maciej <maciej.szulgacz@gmail.com>
2022-02-05 12:33:31 +0100

    create form

---
+++ WorkersApp/forms.py
@@ -0,0 +1,8 @@
+from django.forms import ModelForm
+from .models import Worker
+
+
+class WorkerCreateForm(ModelForm):
+    class Meta:
+        model = Worker
+        fields = ['name', 'surname', 'age', 'profession']
