92933bbe19892e16b7d98c0df6bfb085abb07b35
Maciej <maciej.szulgacz@gmail.com>
2022-02-05 12:30:54 +0100

    create model

--- WorkersApp/models.py
+++ WorkersApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Worker:
+    name = models.CharField(max_length=24)
+    surname = models.CharField(max_length=24)
+    age = models.SmallIntegerField()
+    profession = models.CharField(max_length=50)
+    picture = models.ImageField()
+
+
